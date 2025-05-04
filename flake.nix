cbc2098c4a1599d93c2af3c5d0594a7bda210a84
Zane Dufour <zane@znd4.dev>
2025-05-04 18:50:45 -0400

    add github actions build

--- flake.nix
+++ flake.nix
@@ -4,31 +4,120 @@
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixos-generators = {
+      url = "github:nix-community/nixos-generators";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      imports = [
-        # To import a flake module
-        # 1. Add foo to inputs
-        # 2. Add foo as a parameter to the outputs function
-        # 3. Add here: foo.flakeModule
+  outputs =
+    inputs@{
+      flake-parts,
+      nixpkgs,
+      nixos-generators,
+      self,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      top@{
+        config,
+        withSystem,
+        moduleWithSystem,
+        ...
+      }:
+      {
+        imports = [
+          # To import a flake module
+          # 1. Add foo to inputs
+          # 2. Add foo as a parameter to the outputs function
+          # 3. Add here: foo.flakeModule
 
-      ];
-      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
-      perSystem = { config, self', inputs', pkgs, system, ... }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
+        ];
+        systems = [
+          "x86_64-linux"
+          "aarch64-linux"
+          "aarch64-darwin"
+          "x86_64-darwin"
+        ];
+        perSystem =
+          {
+            config,
+            self',
+            inputs',
+            pkgs,
+            system,
+            ...
+          }:
+          {
+            # Per-system attributes can be defined here. The self' and inputs'
+            # module parameters provide easy access to attributes of the same
+            # system.
 
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-        packages.default = pkgs.hello;
-      };
-      flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
+            # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+            packages.default = pkgs.hello;
+          };
+        flake = {
+          # The usual flake attributes can be defined here, including system-
+          # agnostic ones like nixosModule and system-enumerating ones, although
+          # those are more easily expressed in perSystem.
+          nixosModules.corednsMachine = {
+            imports = [
+              nixos-generators.nixosModules.all-formats
+              (
+                { ... }:
+                {
+                  system.stateVersion = "24.11";
+                }
+              )
+              (
+                { pkgs, lib, ... }:
+                {
+                  services.coredns.enable = true;
+                  # services.coredns.package = pkgs.coredns.override {
+                  #   externalPlugins = [
+                  #     {
+                  #       name = "blocklist";
+                  #       repo = "github.com/relekang/coredns-blocklist";
+                  #       version = "v1.13.0";
+                  #     }
+                  #   ];
+                  #   # fake (placeholder) SRI hash
+                  #   # vendorHash =pkgs.lib.fakeSha256;
+                  #   # fake SRI hash
+                  #   vendorHash = "sha256-uf1qj2JgYF7kCTx0Iuguoo8t7EZjBKGULvg7+j19D8s=";
+                  # };
+                  services.coredns.config = ''
+                    . {
+                      log
+                      prometheus
 
-      };
-    };
+
+                      # load from url
+                      blocklist https://mirror1.malwaredomains.com/files/justdomains {
+                        # if CoreDNS listens at 53, you need another DNS to bootstrap the download
+                        bootstrap_dns 1.1.1.1:53
+                      }
+
+                      # blocklist blocklist.txt
+
+                      forward . 1.1.1.1 1.0.0.1
+                    }
+                  '';
+                }
+              )
+            ];
+            nixpkgs.hostPlatform = "aarch64-linux";
+            # # customize an existing format
+            # formatConfigs.vmware =
+            #   { config, ... }:
+            #   {
+            #     services.openssh.enable = true;
+            #   };
+          };
+          nixosConfigurations.corednsMachine = nixpkgs.lib.nixosSystem {
+            modules = [ self.nixosModules.corednsMachine ];
+          };
+        };
+      }
+    );
 }
